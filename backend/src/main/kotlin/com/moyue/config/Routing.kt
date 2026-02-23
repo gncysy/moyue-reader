ec77697441d91c1d40cc80044304db5198a7173f
gncysy <youxiang20110927@163.com>
2026-02-23 17:34:50 +0800

    Create Routing.kt

---
+++ backend/src/main/kotlin/com/moyue/config/Routing.kt
@@ -0,0 +1,27 @@
+package com.moyue.config
+ 
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import mu.KotlinLogging
+ 
+/**
+ * 配置所有路由
+ */
+fun Application.configureRouting() {
+    val logger = KotlinLogging.logger {}
+    
+    routing {
+        // 健康检查路由
+        healthRoutes()
+        
+        // 书籍路由
+        bookRoutes()
+        
+        // 书源路由
+        sourceRoutes()
+        
+        // TODO: 添加其他路由
+        
+        logger.info { "路由配置完成" }
+    }
+}
